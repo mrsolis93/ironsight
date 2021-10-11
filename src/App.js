fd587d69ce79ed703aadf090ff5d40399376e5ff
tyleraharrison <tyleraharrison@gmail.com>
2021-10-10 21:11:02 -0500

    Add web dashboard

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import logo from './logo.svg';
+import Iframe from 'react-iframe'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Ironsight - Systems Analysis and Design <br></br>
           Under Construction 2021
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Iframe url="https://netdata.tylerharrison.dev/"
+        width="100%"
+        height="700px"
+        id="myId"
+        className="myClassname"
+        display="initial"
+        position="relative"/>
       </header>
     </div>
   );
 }
+
 
 export default App;
