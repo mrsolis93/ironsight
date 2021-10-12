2bf8c25813fc977c054945267a98b86c764af330
tyleraharrison <tyleraharrison@gmail.com>
2021-10-12 00:22:54 -0500

    Updated iFrame

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import logo from './logo.svg';
-import Iframe from 'react-iframe'
+import React from 'react'
 import './App.css';
 
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
         <p>
-          Ironsight - Systems Analysis and Design <br></br>
-          Under Construction 2021
+          Ironsight - Systems Analysis and Design
         </p>
-        <Iframe url="https://netdata.tylerharrison.dev/"
-        width="100%"
-        height="700px"
-        id="myId"
-        className="myClassname"
-        display="initial"
-        position="relative"/>
       </header>
+      <iframe className="Netdata" title="netdata" frameBorder="0" src="https://netdata.tylerharrison.dev/"></iframe>
     </div>
   );
 }
-
 
 export default App;
