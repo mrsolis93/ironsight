4d9123274924518319c613f7fea15793cd499835
tyleraharrison <tyleraharrison@gmail.com>
2022-03-06 20:15:37 -0600

    Changed formatting of App.js

--- src/App.js
+++ src/App.js
@@ -1,40 +1,36 @@
-
-import './App.css';
-import Sidebar from './Components/Sidebar';
-import ReactDOM from 'react-dom';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import Home from './Pages/Home'
-import Analysis from './Pages/Analysis'
-import Labs from './Pages/Labs'
-import Resources from './Pages/Resources'
-import Sandbox from './Pages/Sandbox'
-import Users from './Pages/Users'
+import "./App.css";
+import Sidebar from "./Components/Sidebar";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import Analysis from "./Pages/Analysis";
+import Labs from "./Pages/Labs";
+import Resources from "./Pages/Resources";
+import Sandbox from "./Pages/Sandbox";
+import Users from "./Pages/Users";
 
 function App() {
   return (
     <div className="App">
-     <Router>   
+      <Router>
         <Sidebar />
         <Routes>
-            <Route path='/' exact element={<Home/>} />
-            <Route path='/home' exact element={<Home/>} />
-            <Route path='/analysis' element={<Analysis/>}/>
-            <Route path='/analysis/analysis2' element={<Analysis/>} />
-            <Route path='/labs' element={<Labs/>}/>
-            <Route path='/resources' element={<Resources/>} />
-            <Route path='/sandbox' element={<Sandbox/>}/>
-            <Route path='/users' element={<Users/>}/>
-            {/* <Route path='/users/reports' element={<Reports/>} /> */}
+          <Route path="/" exact element={<Home />} />
+          <Route path="/home" exact element={<Home />} />
+          <Route path="/analysis" element={<Analysis />} />
+          <Route path="/analysis/analysis2" element={<Analysis />} />
+          <Route path="/labs" element={<Labs />} />
+          <Route path="/resources" element={<Resources />} />
+          <Route path="/sandbox" element={<Sandbox />} />
+          <Route path="/users" element={<Users />} />
+          {/* <Route path='/users/reports' element={<Reports/>} /> */}
         </Routes>
       </Router>
-         
-
     </div>
   );
 }
 
-
-const  rootElement = document.getElementById('root');
-ReactDOM.render(<div className="fill-window">{<App />}</div> , rootElement);
+const rootElement = document.getElementById("root");
+ReactDOM.render(<div className="fill-window">{<App />}</div>, rootElement);
 
 export default App;
