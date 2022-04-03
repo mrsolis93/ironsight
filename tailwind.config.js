6453202b4ebeb4a2c043cc18e029ddabaebb3576
tyleraharrison <tyleraharrison@gmail.com>
2022-04-03 05:05:35 -0500

    Fixed spacing on navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ const colors = require('tailwindcss/colors')
 module.exports = {
   enabled: true,
   mode: 'jit',
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: 'class', // class, 'media' or boolean
   theme: {
     extend: {
