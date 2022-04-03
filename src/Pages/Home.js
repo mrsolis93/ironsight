9a818d72c43007c29b8ded963f57b2b6dfc9546e
tyleraharrison <tyleraharrison@gmail.com>
2022-04-03 05:40:09 -0500

    Minor fixes

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -3,7 +3,6 @@ import '../App.css';
 import UsersWidget from '../Components/Widgets/UsersWidget';
 import WidgetTemplate from '../Components/Widgets/WidgetTemplate';
 import RecordsWidget from '../Components/Widgets/RecordsWidget';
-import {Rnd} from 'react-rnd';
 
 
 class Home extends Component {
