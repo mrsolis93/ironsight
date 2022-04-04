00fc9569aae3a8dc0a1d79dbb649c81d3e8d702a
tyleraharrison <tyleraharrison@gmail.com>
2022-04-04 03:10:32 -0500

    Changed users to have DaisyUI

--- src/Components/Widgets/UsersWidget.js
+++ src/Components/Widgets/UsersWidget.js
@@ -1,103 +1,39 @@
-import * as React from "react";
-import Box from "@mui/material/Box";
-import ListItem from "@mui/material/ListItem";
-import ListItemButton from "@mui/material/ListItemButton";
-import ListItemText from "@mui/material/ListItemText";
-import { FixedSizeList } from "react-window";
-import { Rnd } from "react-rnd";
-import { useEffect, useState } from "react";
+import React from "react";
 
-// Get all of the names from key "user_name" in the json from the API call and display them in a list using react-window and MUI
-// URL: "https://api.rellis.dev/get.php?q=get_users"
+const UsersWidget = () => {
+  const [user_list, setUserList] = React.useState("");
 
-function renderRow({ index, style }) {
-  // const { index, style } = props;
-  return (
-    // List all of the names
-    <ListItem style={style} key={index} component="div" disablePadding>
-      <ListItemButton>
-        <ListItemText primary={names[index]} />
-      </ListItemButton>
-    </ListItem>
-  );
-}
-
-var names = [];
-
-function UsersWidget() {
-  const [namesState, setNames] = useState([]);
-
-  var baseUrl = "https://api.rellis.dev/get.php?q=get_users";
-
-  useEffect(() => {
-    const fetchData = async () => {
-      fetch(`${baseUrl}`, {
-        method: "GET",
-        headers: {
-          // 'Content-Type': 'application/json',
-        },
-      })
-        .then((response) => {
-          response.json().then((json) => {
-            // console.log(json)
-            // Get all of the names from key "user_name"
-            names = json.map(function (x) {
-              return x.user_name;
-            });
-            // Set the list of names
-            setNames(names);
-            // console.log(names);
-            // console.log all of the keys in the names
-          });
-        })
-        .catch((error) => {
-          console.log(error);
+  const get_users = () => {
+    fetch("https://api.rellis.dev/get.php?q=get_users")
+      .then((response) => response.json())
+      .then((data) => {
+        // console.log(data);
+        var user_list = data.map(function (user) {
+          return (
+            <tr class="hover">
+              <td>{user.user_name}</td>
+            </tr>
+          );
         });
-    };
-    fetchData();
-  }, [baseUrl]);
+        setUserList(user_list);
+      });
+  };
+  React.useEffect(() => {
+    get_users();
+  }, []);
 
   return (
-    <Rnd
-      default={{
-        x: 970,
-        y: 10,
-        width: 350,
-        height: 350,
-      }}
-    >
-      <Box
-        // Add rounded grey border
-        borderRadius={1}
-        border={7}
-        // borderTop={40}
-        
-        borderColor="#2f3136"
-        borderStyle="solid"
-        sx={{ bgcolor: "transparent" }}
-        // Minimum width of the list
-        minWidth={350}
-      >
-        <Box
-          sx={{ bgcolor: "#2f3136" }}
-          height={30}
-          borderStyle="solid"
-          borderLeft={10}
-          borderColor="#2f3136"
-        >
-          <strong>Users</strong>
-        </Box>
-        <FixedSizeList
-          height={400}
-          // width={200}
-          itemSize={60}
-          itemCount={names.length}
-        >
-          {renderRow}
-        </FixedSizeList>
-      </Box>
-    </Rnd>
+    <div class="md:w-1/4 rounded-box bg-base-100 shadow-xl m-3">
+      <div class="card-body">
+        <h2 class="card-title">Users</h2>
+        <div class="overflow-x-auto max-h-64">
+          <table class="table w-full">
+            <tbody>{user_list}</tbody>
+          </table>
+        </div>
+      </div>
+    </div>
   );
-}
+};
 
 export default UsersWidget;
