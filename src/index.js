ef7ba99351f9e0b062a637340f7e73985eb24f4e
tyleraharrison <tyleraharrison@gmail.com>
2022-04-05 16:48:44 -0500

    Upgraded render to React 18

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { useEffect } from "react";
 
-ReactDOM.render(
-  <>
-    <App />
-  </>,
-  document.getElementById("root")
-);
+function AppWithCallbackAfterRender() {
+  useEffect(() => {
+    console.log("[Ironsight] App rendered");
+  });
+
+  return <App tab="home" />
+}
+
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+root.render(<AppWithCallbackAfterRender />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
