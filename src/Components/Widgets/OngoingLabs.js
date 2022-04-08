e2ae905a7c543599b664fd03cb95056fd6858927
tyleraharrison <tyleraharrison@gmail.com>
2022-04-08 04:16:35 -0500

    Caching ongoing labs

--- src/Components/Widgets/OngoingLabs.js
+++ src/Components/Widgets/OngoingLabs.js
@@ -1,57 +1,59 @@
 import React from "react";
+import { useQuery } from "react-query";
+import { getLabList } from "../../IronsightAPI";
+import LinearProgress from "@mui/material/LinearProgress";
 
 const OngoingLabs = () => {
-  const [lab_list, setLabList] = React.useState([]);
-  const get_labs = () => {
-    fetch("https://api.rellis.dev/get.php?q=get_labs")
-      .then((response) => response.json())
-      .then((data) => {
-        // console.log(data);
-        var lab_list = data.map(function (lab) {
-          // Lab date_start and date_end are in the format of YYYY-MM-DD HH:MM:SS
-          // If the lab is ongoing, it is between date_start and date_end
-          // If the lab is finished, it is after date_end
-          var date_start = new Date(lab.date_start);
-          var date_end = new Date(lab.date_end);
-          var today = new Date();
-          var time_left = date_end - today;
-          // Convert to human readable format (days, hours, minutes)
-          var time_left_readable = { days: 0, hours: 0, minutes: 0 };
-          time_left_readable.days = Math.floor(time_left / (1000 * 60 * 60 * 24));
+  const { data, isLoading, isError } = useQuery("lab_list", getLabList);
 
-          if (today >= date_start && today <= date_end) {
-            return (
-              <tr key={lab.lab_num} className="hover">
-                <th>{lab.lab_num}</th>
-                <td>{lab.lab_name}</td>
-                <td>CSCI 458</td>
-                <td>{time_left_readable.days} days</td>
-              </tr>
-            );
-          }
-        });
-        setLabList(lab_list);
-      });
-  };
-  React.useEffect(() => {
-    get_labs();
-  }, []);
+  if (isLoading) {
+    console.log("[Ironsight] Fetching Chart Data...");
+    return <LinearProgress />;
+  }
 
-  return (
-    <div className="overflow-auto">
-      <table className="table w-full">
-        <thead>
-          <tr>
-            <th></th>
-            <th>Name</th>
-            <th>Class</th>
-            <th>Time Left</th>
-          </tr>
-        </thead>
-        <tbody>{lab_list}</tbody>
-      </table>
-    </div>
-  );
+  if (isError) {
+    return <p>Error!</p>;
+  }
+
+  var lab_list = data.map(function (lab) {
+    // Lab date_start and date_end are in the format of YYYY-MM-DD HH:MM:SS
+    // If the lab is ongoing, it is between date_start and date_end
+    // If the lab is finished, it is after date_end
+    var date_start = new Date(lab.date_start);
+    var date_end = new Date(lab.date_end);
+    var today = new Date();
+    var time_left = date_end - today;
+    // Convert to human readable format (days, hours, minutes)
+    var time_left_readable = { days: 0, hours: 0, minutes: 0 };
+    time_left_readable.days = Math.floor(time_left / (1000 * 60 * 60 * 24));
+
+    if (today >= date_start && today <= date_end) {
+      return (
+        <tr key={lab.lab_num} className="hover">
+          <th>{lab.lab_num}</th>
+          <td>{lab.lab_name}</td>
+          <td>CSCI 458</td>
+          <td>{time_left_readable.days} days</td>
+        </tr>
+      );
+    }
+  });
+
+    return (
+      <div className="overflow-auto">
+        <table className="table w-full">
+          <thead>
+            <tr>
+              <th></th>
+              <th>Name</th>
+              <th>Class</th>
+              <th>Time Left</th>
+            </tr>
+          </thead>
+          <tbody>{lab_list}</tbody>
+        </table>
+      </div>
+    );
 };
 
 export default OngoingLabs;
--- src/IronsightAPI.js
+++ src/IronsightAPI.js
@@ -7,6 +7,15 @@ export const getVMList = async () => {
   return response.json();
 };
 
+export const getLabList = async () => {
+  const response = await fetch(`${process.env.REACT_APP_API_SERVER}get_labs`);
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch labs");
+  }
+  return response.json();
+};
+
 export const getDocCount = async () => {
   const response = await fetch(`${process.env.REACT_APP_API_SERVER}%27{%22size%22:100,%22aggs%22:{%22hostnames%22:{%22terms%22:{%22field%22:%22host.name%22,%22size%22:100}}}}%27`);
 
