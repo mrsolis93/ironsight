24655f44b72a289b68592b8e58ea94e972a2baac
tyleraharrison <tyleraharrison@gmail.com>
2022-04-18 00:59:14 -0500

    Fix loading and component mess

--- src/Components/UserComponents/UserData.js
+++ src/Components/UserComponents/UserData.js
@@ -1,9 +1,12 @@
 import React from "react";
 import { useQuery } from "react-query";
-import { getVMList, getHarvesterVMList, getLabList, getUsersList } from "../../IronsightAPI";
+import {
+  getHarvesterVMList,
+  getLabList,
+  getUsersList,
+} from "../../IronsightAPI";
 import LinearProgress from "@mui/material/LinearProgress";
 import { Link } from "react-router-dom";
-
 
 // const newUser = () => {
 
@@ -12,110 +15,91 @@ import { Link } from "react-router-dom";
 //     classes: namor.generate({ words: 1, numbers: 0 }),
 //     labs: Math.floor(Math.random() * 30),
 //     vms: Math.floor(Math.random() * 30),
- 
+
 //   }
 // }
 
 const useUserData = () => {
+  const { data, isLoading, isError } = useQuery("harvester_vms", getHarvesterVMList);
+  const {
+    data: harvester_data,
+    isLoading: harvester_isLoading,
+    isError: harvester_isError,
+  } = useQuery("harvester_vms", getHarvesterVMList);
 
-    const { data, isLoading, isError } = useQuery("virtual_machines", getVMList);
-    const {
-      data: harvester_data,
-      isLoading: harvester_isLoading,
-      isError: harvester_isError,
-    } = useQuery("harvester_vms", getHarvesterVMList, {
-      refetchInterval: 5000,
-    });
+  const {
+    data: lab_data,
+    isLoading: lab_isLoading,
+    isError: lab_isError,
+  } = useQuery("labs", getLabList);
 
-    const {
-      data: lab_data,
-      isLoading: lab_isLoading,
-      isError: lab_isError,
-    } = useQuery("labs", getLabList, {
-      refetchInterval: 5000,
-    });
+  const {
+    data: users_data,
+    isLoading: users_isLoading,
+    isError: users_isError,
+  } = useQuery("users", getUsersList);
 
-    const {
-      data: users_data,
-      isLoading: users_isLoading,
-      isError: users_isError,
-    } = useQuery("users", getUsersList, {
-      refetchInterval: 5000,
-    });
+  if (isLoading || harvester_isLoading || lab_isLoading || users_isLoading) {
+    return;
+  }
 
-    if (isLoading || harvester_isLoading || lab_isLoading || users_isLoading) {
-      return <LinearProgress />;
+  if (isError || harvester_isError || lab_isError || users_isError) {
+    return;
+  }
+
+  const get_vm_list_length = () => {
+    return data.length;
+  };
+  const vm_list_length = get_vm_list_length();
+
+  const get_labs_list_length = () => {
+    return lab_data.length;
+  };
+  const labs_length = get_labs_list_length();
+
+  var raw_student_data = [];
+
+  raw_student_data = users_data;
+  // console.log(raw_student_data);
+  var table_html = raw_student_data.map(function (student) {
+    // Capitalize the first letter of the first name
+    var first_name =
+      student.first_name.charAt(0).toUpperCase() + student.first_name.slice(1);
+
+    // Capitalize the first letter of the last name
+    var last_name =
+      student.last_name.charAt(0).toUpperCase() + student.last_name.slice(1);
+
+    var student_email = student.user_name + "@leomail.tamuc.edu";
+    // Check the tags to see if the student is a student or a professor
+    var user_role = "";
+    for (let i = 0; i < student.tags.length; i++) {
+      if (student.tags[i]["type"] === "role") {
+        user_role = student.tags[i]["tag"];
+      }
     }
 
-    if (isError || harvester_isError || lab_isError || users_isError) {
-      return <p>Error!</p>;
+    // Check for a link to a profile picture
+    var profile_pic_data = "";
+    if (student.profile_pic_data !== null) {
+      profile_pic_data = student["profile_pic_data"];
+    } else {
+      profile_pic_data =
+        "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__480.png";
     }
 
-    const get_vm_list_length = () => {
-  
-      return data.length;
+    var student_major = "";
 
-    };
-    const vm_list_length = get_vm_list_length();
-
-
-
-    const get_labs_list_length = () => {
-
-      return lab_data.length;
-
-    };
-    const labs_length = get_labs_list_length();
-
-
-
-    
-    var raw_student_data = [];
-      
-    raw_student_data = users_data;
-    var table_html = raw_student_data.map(function (student) 
-    {
-
-        // Capitalize the first letter of the first name
-        var first_name =
-          student.first_name.charAt(0).toUpperCase() + student.first_name.slice(1);
-
-        // Capitalize the first letter of the last name
-        var last_name =
-          student.last_name.charAt(0).toUpperCase() + student.last_name.slice(1);
-
-        var student_email = student.user_name + "@leomail.tamuc.edu";
-        // Check the tags to see if the student is a student or a professor
-        var user_role = "";
-        for (let i = 0; i < student.tags.length; i++) {
-          if (student.tags[i]["type"] === "role") {
-            user_role = student.tags[i]["tag"];
-          }
-        }
-
-      // Check for a link to a profile picture
-      var profile_pic_data = "";
-      if (student.profile_pic_data !== null) {
-          profile_pic_data = student["profile_pic_data"];
+    for (let i = 0; i < student.tags.length; i++) {
+      if (student.tags[i]["type"] === "major") {
+        student_major = student.tags[i]["tag"];
       }
+    }
+  });
 
-      else {
-          profile_pic_data = "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__480.png";
-      }
+  //TEST push data from the API to an array for the table
 
-      var student_major = "";
-
-      for (let i = 0; i < student.tags.length; i++) {
-          if (student.tags[i]["type"] === "major") {
-              student_major = student.tags[i]["tag"];
-          }
-      }  
-
-    });
-
-//TEST push data from the API to an array for the table
-
- var table_data = [];
+  var table_data = [];
   for (let i = 0; i < raw_student_data.length; i++) {
     table_data.push({
       first_name: raw_student_data[i]["first_name"],
@@ -127,13 +111,8 @@ const useUserData = () => {
     });
   }
 
-  console.log("table_data", table_data);
-
   // return useUserData in the form of an table_data array
-  return {
-    table_data
-  }
-}
-
+  return table_data;
+};
 
 export default useUserData;
--- src/Components/UserComponents/UserTable.js
+++ src/Components/UserComponents/UserTable.js
@@ -1,83 +1,77 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
-import CssBaseline from '@material-ui/core/CssBaseline'
-import EnhancedTable from './EnhancedTable'
-import useUserData, {table_html} from './UserData'
-
-
+import CssBaseline from "@material-ui/core/CssBaseline";
+import EnhancedTable from "./EnhancedTable";
+import useUserData from "./UserData";
+import LinearProgress from "@mui/material/LinearProgress";
 
 function UserTable() {
-
-    const columns = React.useMemo(
-        () => [
-          
-          {
-            Header: 'User',
-            accessor: 'user',
-
-          },
-          {
-            Header: 'Role',
-            accessor: 'role',
-          },
-          {
-            Header: 'Class',
-            accessor: 'class',
-          },
-          {
-            Header: '# of Labs',
-            accessor: 'labs',
-          },
-       
-          {
-            Header: '# of VMs',
-            accessor: 'vms',
-          },
-        ],
-        []
-      )
-
-      const [data, setData] = useState([]);
-      const [skipPageReset, setSkipPageReset] = React.useState(false)
-    
-      // We need to keep the table from resetting the pageIndex when we
-      // Update data. So we can keep track of that flag with a ref.
-    
-      // When our cell renderer calls updateMyData, we'll use
-      // the rowIndex, columnId and new value to update the
-      // original data
-      
-
-      
-      // const user_data = useUserData();
-
-      // //set user data to data
-      // setData(user_data);
-
-      // console.log("UserData: ", user_data);
-
-      const updateMyData = (rowIndex, columnId, value) => 
-      
+  const columns = React.useMemo(
+    () => [
       {
-        // We also turn on the flag to not reset the page
-        setSkipPageReset(true)
-        setData(old =>
-          old.map((row, index) => {
-            if (index === rowIndex) {
-              return {
-                ...old[rowIndex],
-                [columnId]: value,
-              }
-            }
-            return row
+        Header: "User",
+        accessor: "user",
+      },
+      {
+        Header: "Role",
+        accessor: "role",
+      },
+      {
+        Header: "Class",
+        accessor: "class",
+      },
+      {
+        Header: "# of Labs",
+        accessor: "labs",
+      },
 
-          })
-        )
-      }
+      {
+        Header: "# of VMs",
+        accessor: "vms",
+      },
+    ],
+    []
+  );
+
+  const [data, setData] = useState([]);
+  const [skipPageReset, setSkipPageReset] = React.useState(false);
+
+  // We need to keep the table from resetting the pageIndex when we
+  // Update data. So we can keep track of that flag with a ref.
+
+  // When our cell renderer calls updateMyData, we'll use
+  // the rowIndex, columnId and new value to update the
+  // original data
+
+  const user_data = useUserData();
+
+  // //set user data to data
+  // setData(user_data);
+
+  if (user_data) {
+  console.log("UserData: ", user_data);
+  }
+  else {
+    return <LinearProgress />;
+  }
+
+  const updateMyData = (rowIndex, columnId, value) => {
+    // We also turn on the flag to not reset the page
+    setSkipPageReset(true);
+    setData((old) =>
+      old.map((row, index) => {
+        if (index === rowIndex) {
+          return {
+            ...old[rowIndex],
+            [columnId]: value,
+          };
+        }
+        return row;
+      })
+    );
+  };
 
   return (
-
-
     <div>
       <CssBaseline />
       <EnhancedTable
@@ -105,8 +99,7 @@ function UserTable() {
     //       </tbody>
     //     </table>
     // </div>
-
-  )
+  );
 }
 
-export default UserTable
+export default UserTable;
