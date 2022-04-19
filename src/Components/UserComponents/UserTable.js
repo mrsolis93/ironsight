a124e988fe50306bddca965d423ae687784389dd
mrsolis93 <mr.solis93@yahoo.com>
2022-04-19 05:12:40 -0500

    The Table Saga Continues

--- src/Components/UserComponents/UserTable.js
+++ src/Components/UserComponents/UserTable.js
@@ -1,107 +1,178 @@
-import React, { useState } from "react";
+import * as React from 'react';
 
-import CssBaseline from "@material-ui/core/CssBaseline";
-import EnhancedTable from "./EnhancedTable";
-import useUserData from "./UserData";
+import { useQuery } from "react-query";
+import {
+  getUsersList,
+} from "../../IronsightAPI";
+ 
+ 
+
+import { CompactTable } from '@table-library/react-table-library/compact';
+import { useTheme } from '@table-library/react-table-library/theme';
+import { DEFAULT_OPTIONS, getTheme } from '@table-library/react-table-library/material';
+import { useSort } from '@table-library/react-table-library/sort';
+import { FaChevronDown, FaChevronUp } from 'react-icons/fa';
+import { Stack, TextField } from '@mui/material';
+import { FaSearch } from 'react-icons/fa';
+// import {useUserData} from "./UserData";
 import LinearProgress from "@mui/material/LinearProgress";
 
-function UserTable() {
-  const columns = React.useMemo(
-    () => [
-      {
-        Header: "User",
-        accessor: "first_name",
-      },
-      {
-        Header: "Role",
-        accessor: "last_name",
-      },
-      {
-        Header: "Class",
-        accessor: "user_name",
-      },
-      {
-        Header: "# of Labs",
-        accessor: "profile_pic_data",
-      },
+ import { nodes } from './data';
 
-      {
-        Header: "# of VMs",
-        accessor: "student_major",
-      }, 
-      {
-        Header: "roles",
-        accessor: "user_role",
-      },
-    ],
-    []
-  );
-
-  const user_data = useUserData();
-
-  const [data, setData] = useState(React.useMemo(() => user_data, []))
-  const [skipPageReset, setSkipPageReset] = React.useState(false);
-
-  // We need to keep the table from resetting the pageIndex when we
-  // Update data. So we can keep track of that flag with a ref.
-
-  // When our cell renderer calls updateMyData, we'll use
-  // the rowIndex, columnId and new value to update the
-  // original data
+const key = 'Search';
+ 
+const UserTable = () => {
 
 
-  if (user_data) {
-  console.log("UserData: ", user_data);
-  }
-  else {
-    return <LinearProgress />;
-  }
+  const {
+    data: users_data,
+    isLoading: users_isLoading,
+    isError: users_isError,
+  } = useQuery("users_list", getUsersList);
 
-  const updateMyData = (rowIndex, columnId, value) => {
-    // We also turn on the flag to not reset the page
-    setSkipPageReset(true);
-    setData((old) =>
-      old.map((row, index) => {
-        if (index === rowIndex) {
-          return {
-            ...old[rowIndex],
-            [columnId]: value,
-          };
-        }
-        return row;
-      })
-    );
+  console.log("Users Data:",users_data)
+
+  //set data to users_data
+  let data =  table_data ;
+
+  console.log("Data:",data)
+  console.log("Top Table Data:",table_data)
+
+  const materialTheme = getTheme(DEFAULT_OPTIONS);
+  const theme = useTheme(materialTheme);
+
+  const [search, setSearch] = React.useState('');
+
+  const handleSearch = (event) => {
+    setSearch(event.target.value);
   };
 
-  return (
-    <div>
-      <CssBaseline />
-      <EnhancedTable
-        columns={columns}
-        data={data}
-        setData={setData}
-        updateMyData={updateMyData}
-        skipPageReset={skipPageReset}
-      />
-    </div>
 
-    // <div className="overflow-auto m-20 xl:mx-48">
-    //  <table className="table w-full">
-    //       <thead>
-    //         <tr>
-    //           <th>User</th>
-    //           <th>Class</th>
-    //           <th>Labs</th>
-    //           <th>Progress</th>
-    //           <th>VirtualMachines</th>
-    //         </tr>
-    //       </thead>
-    //       <tbody>
-    //         \\
-    //       </tbody>
-    //     </table>
-    // </div>
+  const sort = useSort(
+    data,
+    {
+      onChange: onSortChange,
+    },
+    {
+      sortIcon: {
+        iconDefault: null,
+        iconUp: <FaChevronUp />,
+        iconDown: <FaChevronDown />,
+      },
+      sortFns: {
+        TASK: (array) => array.sort((a, b) => a.name.localeCompare(b.name)),
+        DEADLINE: (array) => array.sort((a, b) => a.deadline - b.deadline),
+        TYPE: (array) => array.sort((a, b) => a.type.localeCompare(b.type)),
+        COMPLETE: (array) => array.sort((a, b) => a.isComplete - b.isComplete),
+        TASKS: (array) => array.sort((a, b) => (a.nodes || []).length - (b.nodes || []).length),
+      },
+    },
   );
-}
+
+  function onSortChange(action, state) {
+    console.log(action, state);
+  }
+
+  if ( users_isLoading ) {
+    console.log("Users Loading...");
+    return "Hello";
+
+  }
+
+  if (   users_isError) {
+    console.log("Error...");
+    return "Error";
+  }
+
+  var raw_student_data = [];
+
+  raw_student_data = users_data;
+  // console.log(raw_student_data);
+  var table_html = raw_student_data.map(function (student) {
+    // Capitalize the first letter of the first name
+    var first_name =
+      student.first_name.charAt(0).toUpperCase() + student.first_name.slice(1);
+
+    // Capitalize the first letter of the last name
+    var last_name =
+      student.last_name.charAt(0).toUpperCase() + student.last_name.slice(1);
+
+    var student_email = student.user_name + "@leomail.tamuc.edu";
+    // Check the tags to see if the student is a student or a professor
+    var user_role = "";
+    for (let i = 0; i < student.tags.length; i++) {
+      if (student.tags[i]["type"] === "role") {
+        user_role = student.tags[i]["tag"];
+      }
+    }
+
+    // Check for a link to a profile picture
+    var profile_pic_data = "";
+    if (student.profile_pic_data !== null) {
+      profile_pic_data = student["profile_pic_data"];
+    } else {
+      profile_pic_data =
+        "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__480.png";
+    }
+
+    var student_major = "";
+
+    for (let i = 0; i < student.tags.length; i++) {
+      if (student.tags[i]["type"] === "major") {
+        student_major = student.tags[i]["tag"];
+      }
+    }
+  });
+
+  var table_data = [];
+  for (let i = 0; i < raw_student_data.length; i++) {
+    table_data.push({
+      first_name: raw_student_data[i]["first_name"],
+      last_name: raw_student_data[i]["last_name"],
+      user_name: raw_student_data[i]["user_name"],
+      profile_pic_data: raw_student_data[i]["profile_pic_data"],
+      student_major: raw_student_data[i]["tags"][1]["tag"],
+      user_role: raw_student_data[i]["tags"][0]["tag"],
+    });
+  }
+
+  console.log("Table Data: ", data.table_data);
+  
+  data = {
+    table_data: data.table_data.filter((item) => item.name.toLowerCase().includes(search.toLowerCase())),
+  };
+
+  console.log("Table Data 2: ", table_data);
+
+  const COLUMNS = [
+    { label: 'User', renderCell: (item) => item.first_name, sort: { sortKey: 'TASK' } },
+
+    { label: 'Role', renderCell: (item) => item.last_name, sort: { sortKey: 'DEADLINE' }},
+
+    { label: 'Role', renderCell: (item) => item.user_name, sort: { sortKey: 'DEADLINE' }},
+
+    { label: 'Class', renderCell: (item) => item.profile_pic_data, sort: { sortKey: 'TYPE' } },
+
+    { label: '# of Labs', renderCell: (item) => item.student_major, sort: { sortKey: 'COMPLETE' }},
+
+    { label: '# of VMs', renderCell: (item) => item.user_role, sort: { sortKey: 'TASKS' } },
+  ];
+
+  return (
+
+    <>
+      <Stack spacing={10}>
+        <TextField label="Search Task" value={search} icon={<FaSearch />} onChange={handleSearch} />
+      </Stack>
+      <br />
+
+      <CompactTable columns={COLUMNS} data={data} sort={sort} theme={theme} />
+
+      <br />
+      
+    </>
+
+  );
+};
 
 export default UserTable;
--- src/Pages/Users.js
+++ src/Pages/Users.js
@@ -2,23 +2,31 @@ import React from "react";
 import "../App.css";
 import ReAreaChart from "../Charts/ReAreaChart";
 import ReAreaChartMultiple from "../Charts/ReAreaChartMultiple";
-import ReAreaChartMultiple2 from "../Charts/ReAreaChartMutiple2";
+
 import ReLineChart from "../Charts/ReLineChart";
 import Navbar from "../Components/Navbar";
-import EnhancedTable from "../Components/UserComponents/EnhancedTable";
+
 import UserTable from "../Components/UserComponents/UserTable";
 
 function Users() {
   return (
     <div className="users">
+
       <Navbar />
-      
-        <div>
-        <UserTable />
-          
-        </div>
+
+      <div className="overflow-auto m-4 xl:mx-48">
+          <div className="w-full h-full bg-base-100 shadow-xl">
+            <div className="course-content">
+
+              <UserTable />
+                 
+            </div>
+          </div>
+      </div>
 
     </div>
+    
+
   );
 }
 
