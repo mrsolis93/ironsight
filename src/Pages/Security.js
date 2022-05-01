c0e6ddd0731a9bd269c94085037df5168acef68e
tyleraharrison <tyleraharrison@gmail.com>
2022-05-01 01:58:27 -0500

    Added security onion placeholder

--- src/Pages/Security.js
+++ src/Pages/Security.js
@@ -6,7 +6,15 @@ function Security() {
     <>
       <Navbar />
       <div className="security">
-        
+        {/* Insert image https://docs.securityonion.net/en/2.3/_images/39_hunt.png */}
+        {/* Crop the top 20px */}
+        <div className="security-image">
+          <img
+            src="/assets/sec_onion.png"
+            alt="security-image"
+            width="100%"
+          />
+          </div>
       </div>
     </>
   );
