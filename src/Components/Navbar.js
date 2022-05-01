3fe74d877de3801538ce2b9052e3b4b9834513de
tyleraharrison <tyleraharrison@gmail.com>
2022-05-01 01:50:07 -0500

    Fixed scaling with added navbar pages

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -71,9 +71,9 @@ export default function Navbar() {
     <Disclosure as="nav" className="bg-gray-800">
       {({ open }) => (
         <>
-          <div className="mx-auto px-2 sm:px-6 lg:px-8">
+          <div className="mx-auto px-2 lg:px-6 lg:px-8">
             <div className="relative flex items-center justify-between h-16">
-              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+              <div className="absolute inset-y-0 left-0 flex items-center lg:hidden">
                 {/* Mobile menu button*/}
                 <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                   <span className="sr-only">Open main menu</span>
@@ -85,7 +85,7 @@ export default function Navbar() {
                 </Disclosure.Button>
               </div>
 
-              <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
+              <div className="flex-1 flex items-center justify-center lg:items-stretch lg:justify-start">
                 <div className="flex-shrink-0 flex items-center">
                   <img
                     className="block lg:hidden h-8 w-auto"
@@ -99,7 +99,7 @@ export default function Navbar() {
                   />
                 </div>
 
-                <div className="hidden sm:block sm:ml-6">
+                <div className="hidden lg:block lg:ml-6">
                   <DesktopLinks>
                     {NavbarData.map((item) => (
                       <NavLink
@@ -118,7 +118,7 @@ export default function Navbar() {
                 </div>
               </div>
 
-              <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+              <div className="absolute inset-y-0 right-0 flex items-center pr-2 lg:static lg:inset-auto lg:ml-6 lg:pr-0">
                 <ThemeButton />
                 {/* Profile dropdown */}
                 {/* Bring to top of z-depth */}
@@ -166,7 +166,7 @@ export default function Navbar() {
           </div>
 
           {/* Mobile NavBar */}
-          <Disclosure.Panel className="sm:hidden">
+          <Disclosure.Panel className="lg:hidden">
             <div className="px-2 pt-2 pb-3 space-y-1">
               {NavbarData.map((item) => (
                 <MobileLink
