080c62347dbf4aa60211cef6724e482481ccbe18
tyleraharrison <tyleraharrison@gmail.com>
2022-05-01 01:50:55 -0500

    Minor error fixes

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -71,7 +71,7 @@ export default function Navbar() {
     <Disclosure as="nav" className="bg-gray-800">
       {({ open }) => (
         <>
-          <div className="mx-auto px-2 lg:px-6 lg:px-8">
+          <div className="mx-auto px-2 sm:px-6 lg:px-8">
             <div className="relative flex items-center justify-between h-16">
               <div className="absolute inset-y-0 left-0 flex items-center lg:hidden">
                 {/* Mobile menu button*/}
