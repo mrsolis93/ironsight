ab06477683506d85b21ee9f4515d1afc51a65e67
tyleraharrison <tyleraharrison@gmail.com>
2022-05-01 02:14:09 -0500

    Updated Settings page

--- src/Pages/Settings.js
+++ src/Pages/Settings.js
@@ -1,38 +1,97 @@
 import React from "react";
 import "../App.css";
 import Navbar from "../Components/Navbar";
-
 import CreateUser from "../Components/CreateUser";
 import CreateLab from "../Components/CreateLab";
 import CreateCourse from "../Components/CreateCourse";
+import LinearProgress from "@mui/material/LinearProgress";
+import { Link } from "react-router-dom";
 
-const Management = () => {
+function Settings() {
+  const [selectedTab, setSelectedTab] = React.useState("users");
+
+  const set_selected_tab = (tab) => {
+    setSelectedTab(tab);
+  };
+
   return (
-    <div className="testing">
+    <div className="settings">
       <Navbar />
-      <div className="grid grid-cols-1 md:grid-cols-4 grid-flow-row gap-4 m-4">
-        {/* User creation testing */}
-        <div className="col-span-1 rounded-box bg-base-100">
-          <h2 className="collapse-title text-center text-base-900">
-            User Creation
-          </h2>
-          <CreateUser />
+
+      {/* Top bar (breadcrumbs) */}
+      <div className="text-md breadcrumbs m-4">
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <strong>Settings</strong>
+          </li>
+        </ul>
+      </div>
+
+      {/* Sidebar with Labs, Users, and Courses tabs */}
+      <div className="grid grid-cols-5 gap-4 m-4 h-[80%]">
+        <div className="row-span-1 col-span-5 lg:col-span-1">
+          <div className="rounded-box w-full h-full bg-base-100 shadow-xl">
+            <div className="sidebar-links">
+              {/* Display rows but highlight the one using selectedTab */}
+              <div className="grid grid-cols-3 lg:grid-cols-1 grid-flow-row gap-4 mx-4">
+                <button
+                  className={
+                    selectedTab === "users"
+                      ? "btn btn-primary w-full text-base-900 hover cursor-pointer mt-4 mb-4 lg:mb-0"
+                      : "btn btn-outline w-full text-base-900 hover cursor-pointer mt-4 mb-4 md:mb-0"
+                  }
+                  onClick={() => set_selected_tab("users")}
+                >
+                  Users
+                </button>
+                <button
+                  className={
+                    selectedTab === "labs"
+                      ? "btn btn-primary w-full text-base-900 hover cursor-pointer mt-4 lg:mt-0"
+                      : "btn btn-outline w-full text-base-900 hover cursor-pointer mt-4 lg:mt-0"
+                  }
+                  onClick={() => set_selected_tab("labs")}
+                >
+                  Labs
+                </button>
+                <button
+                  className={
+                    selectedTab === "courses"
+                      ? "btn btn-primary w-full text-base-900 hover cursor-pointer mb-4 mt-4 lg:mt-0"
+                      : "btn btn-outline w-full text-base-900 hover cursor-pointer mb-4 mt-4 lg:mt-0"
+                  }
+                  onClick={() => set_selected_tab("courses")}
+                >
+                  Courses
+                </button>
+              </div>
+            </div>
+          </div>
         </div>
-        <div className="col-span-2 rounded-box bg-base-100">
-        <h2 className="collapse-title text-center text-base-900">
-            Lab Creation
-          </h2>
-          <CreateLab />
-        </div>
-        <div className="col-span-1 rounded-box bg-base-100">
-        <h2 className="collapse-title text-center text-base-900">
-            Course Creation
-          </h2>
-          <CreateCourse />
+
+        {/* Page content */}
+        <div className="rounded-box row-span-1 col-span-5 lg:col-span-4 h-full overflow-auto">
+          <div className="w-full h-full bg-base-100 shadow-xl">
+            <div className="course-content">
+              {/* Display the selected tab */}
+
+              {/* Labs */}
+              {selectedTab === "labs" && <CreateLab />}
+
+              {/* Users */}
+              {selectedTab === "users" && <CreateUser />}
+
+              {/* Courses */}
+              {selectedTab === "courses" && <CreateCourse />}
+            </div>
+          </div>
         </div>
       </div>
     </div>
   );
-};
+}
 
-export default Management;
+export default Settings;
